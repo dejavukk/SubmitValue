eb17e6685f315b2de880ad5dd94b27ab550920ad
junhyuk <dejavukk1@gmail.com>
2019-10-30 23:57:51 +0900

    Initial Commit

---
+++ SubmitValue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SubmitValue
+//
+//  Created by JunHyuk on 2019/10/30.
+//  Copyright © 2019 junhyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
