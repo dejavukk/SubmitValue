77dfdab53adae128420f3e5b9546abdb83fd0842
junhyuk <dejavukk1@gmail.com>
2019-11-01 23:45:38 +0900

    자동갱신여부, 갱신주기 바뀔때마다 메소드 호출 기능 구현중.

--- SubmitValue/ViewController.swift
+++ SubmitValue/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 화면의 프로퍼티 선언.
+    @IBOutlet var email: UITextField!
+    @IBOutlet var isUpdate: UISwitch!
+    @IBOutlet var interval: UIStepper!
+    
+    @IBOutlet var isUpdateText: UILabel!
+    @IBOutlet var intervalText: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // 자동 갱신 여부가 바뀔 때마다 호출되는 메소드
+    @IBAction func onSwitch(_ sender: UISwitch) {
+        
+        if (sender.isOn == true) {
+            self.isUpdateText.text = "갱신함"
+        } else {
+            self.isUpdateText.text = "갱신하지 않음"
+        }
+    }
+    
+    // 갱신 주기가 바뀔 때마다 호출되는 메소드
+    @IBAction func onStepper(_ sender: UIStepper) {
+        
+        let value = Int(sender.value)
+        self.intervalText.text = "\(value)분 마다."
+        
+        
+        
+    }
+    
 
 }
 
